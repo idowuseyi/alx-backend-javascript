e1254cf366d9db8e0293ccd6eaf36284dd41dd6c
Oluwaseyi Idowu <106804924+idowuseyi@users.noreply.github.com>
2023-04-18 05:56:34 +0100

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  var emp = {
-    departmentName: $name,
-    employees:
-  }  
-
+  return {
+    [departmentName]: employees,
+  };
 }
